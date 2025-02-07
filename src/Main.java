961d8b5d3406e7587d2ceac673dbe835dbec860a
Pablofd <pablo.fernandezdiaz@edu.uah.es>
2025-02-07 13:40:40 +0100

    ent1_eja_e2 completado

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import ent1.eja.e1.Circle;
+import ent1.eja.e2.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
-        ent1_eja_e1();
+        ent1_eja_e2();
     }
 
     static public void ent1_eja_e1(){
@@ -18,6 +19,24 @@ public class Main {
 // Test getArea() and getCircumference()
         System.out.printf("area is: %.2f%n", c1.getArea());
         System.out.printf("circumference is: %.2f%n", c1.getCircumference());
+    }
+
+    static public void ent1_eja_e2(){
+        // Test constructors and toString()
+// You need to append a 'f' or 'F' to a float literal
+        Rectangle r1 = new Rectangle(1.2f, 3.4f);
+        System.out.println(r1); // toString()
+        Rectangle r2 = new Rectangle(); // default constructor
+        System.out.println(r2);
+// Test setters and getters
+        r1.setLength(5.6f);
+        r1.setWidth(7.8f);
+        System.out.println(r1); // toString()
+        System.out.println("length is: " + r1.getLength());
+        System.out.println("width is: " + r1.getWidth());
+// Test getArea() and getPerimeter()
+        System.out.printf("area is: %.2f%n", r1.getArea());
+        System.out.printf("perimeter is: %.2f%n", r1.getPerimeter());
     }
 
 
---
+++ src/ent1/eja/e2/Rectangle.java
@@ -0,0 +1,38 @@
+package ent1.eja.e2;
+
+public class Rectangle {
+    float length = 1.0f;
+    float width = 1.0f;
+
+    public Rectangle(){}
+    public Rectangle(float length,float width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+    public double getArea(){
+        return length*width;
+    }
+    public double getPerimeter(){
+        return 2*length+2*width;
+    }
+    @Override
+    public String toString(){
+        return "Rectangle[length="+length+",width="+width+"]";
+    }
+}
