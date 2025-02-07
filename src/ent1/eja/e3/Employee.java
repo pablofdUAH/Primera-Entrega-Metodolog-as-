9b95eec14cb9aa629918c1405b6ab1f243cd8231
Pablofd <pablo.fernandezdiaz@edu.uah.es>
2025-02-07 13:52:21 +0100

    ent1_eja_e3 constructor y atributos

---
+++ src/ent1/eja/e3/Employee.java
@@ -0,0 +1,15 @@
+package ent1.eja.e3;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastname;
+    private int salary;
+    public Employee(int id,String firstName,String lastname,int salary){
+        this.id = id;
+        this.firstName=firstName;
+        this.lastname=lastname;
+        this.salary=salary;
+    }
+}
+
