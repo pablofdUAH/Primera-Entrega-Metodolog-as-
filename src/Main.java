e16880716b8baaa9976fc3015f980ad26d6148d0
Pablofd Casa <pablo.fernandezdiaz@edu.uah.es>
2025-02-08 12:40:07 +0100

    ent1_eja_e7 completado

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import ent1.eja.e7.Time;
 
 public class Main {
     public static void main(String[] args) {
-        ent1_eja_e8();
+        ent1_eja_e7();
     }
 
     static public void ent1_eja_e1(){
@@ -141,27 +141,7 @@ public class Main {
         System.out.println(t1.previousSecond().previousSecond());
     }
 
-    static public void ent1_eja_e8(){
-        // Test constructors and toString()
-        Time t1 = new Time(1, 2, 3);
-        System.out.println(t1); // toString()
-// Test Setters and Getters
-        t1.setHour(4);
-        t1.setMinute(5);
-        t1.setSecond(6);
-        System.out.println(t1); // toString()
-        System.out.println("Hour: " + t1.getHour());
-        System.out.println("Minute: " + t1.getMinute());
-        System.out.println("Second: " + t1.getSecond());
-// Test setTime()
-        t1.setTime(23, 59, 58);
-        System.out.println(t1); // toString()
-// Test nextSecond();
-        System.out.println(t1.nextSecond());
-        System.out.println(t1.nextSecond().nextSecond());
-// Test previousSecond()
-        System.out.println(t1.previousSecond());
-        System.out.println(t1.previousSecond().previousSecond());
-    }
+
+
 }
 
