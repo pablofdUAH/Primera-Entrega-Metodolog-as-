aae681df4c32fb7e88ecb027d6f2dd4a538e80a6
Pablofd Casa <pablo.fernandezdiaz@edu.uah.es>
2025-02-08 12:52:32 +0100

    ent1_eja_e8 completado

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,11 @@ import ent1.eja.e4.InvoiceItem;
 import ent1.eja.e5.Account;
 import ent1.eja.e6.Date;
 import ent1.eja.e7.Time;
+import ent1.eja.e8.Ball;
 
 public class Main {
     public static void main(String[] args) {
-        ent1_eja_e7();
+        ent1_eja_e8();
     }
 
     static public void ent1_eja_e1(){
@@ -141,7 +142,42 @@ public class Main {
         System.out.println(t1.previousSecond().previousSecond());
     }
 
+    static public void ent1_eja_e8(){
+        // Test constructor and toString()
+        Ball ball = new Ball(1.1f, 2.2f, 10, 3.3f, 4.4f);
+        System.out.println(ball); // toString()
+// Test Setters and Getters
+        ball.setX(80.0f);
+        ball.setY(35.0f);
+        ball.setRadius(5);
+        ball.setXDelta(4.0f);
+        ball.setYDelta(6.0f);
+        System.out.println(ball); // toString()
+        System.out.println("x is: " + ball.getX());
+        System.out.println("y is: " + ball.getY());
+        System.out.println("radius is: " + ball.getRadius());
+        System.out.println("xDelta is: " + ball.getXDelta());
+        System.out.println("yDelta is: " + ball.getYDelta());
+        float xMin = 0.0f;
+        float xMax = 100.0f;
+        float yMin = 0.0f;
+        float yMax = 50.0f;
+        for (int i = 0; i < 15; i++) {
+            ball.move();
+            System.out.println(ball);
+            float xNew = ball.getX();
+            float yNew = ball.getY();
+            int radius = ball.getRadius();
+// Check boundary value to bounce back
+            if ((xNew + radius) > xMax || (xNew - radius) < xMin) {
+                ball.reflectHorizontal();
+            }
+            if ((yNew + radius) > yMax || (yNew - radius) < yMin) {
+                ball.reflectVertical();
+            }
+        }
 
+    }
 
 }
 
